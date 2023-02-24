566a0457fb60c633500f29d0e8043581944db79d
Travis <travisliu708@gmail.com>
2023-02-23 19:52:50 -0800

    create QRCodeIndex component

---
+++ web/frontend/components/QRCodeIndex.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "@shopify/app-bridge-react";
+import {
+
+} from "@shopify/polaris";
+import { DiamondAlertMajor, ImageMajor} from "@shopify/polaris-icons";
+
+// useMedia for multiple screensizes
+import { useMedia } from "@shopify/react-hooks";
+
+// dayjs for capturing and formatting date 
+import dayjs from 'dayjs';
